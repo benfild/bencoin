af2bd03db8f008ab46e5ed2601dac9c0ab61fbce
benfild <fildben@gmail.com>
2021-11-01 23:01:10 -0400

    modify: update test 3 code

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 const {Blockchain, Transaction} = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+const myKey = ec.keyFromPrivate('bc57f31d4f9c50e6edb76f458a7c2ff2f9e393a255bc699fc7b1e826eecb7a32');
+const myWalletAddress = myKey.getPublic('hex');
 let benCoin = new Blockchain();
 
 //TEST 1
@@ -16,16 +21,21 @@ let benCoin = new Blockchain();
 
 // console.log(JSON.stringify(benCoin, null, 4));
 
+// TEST 3
+const tx1 = new Transaction(myWalletAddress, 'another Public Key', 10);
+tx1.signTransaction(myKey);
+benCoin.addTransaction(tx1);
+
 // TEST 2
-benCoin.addTransaction(new Transaction('address1', 'address2', 150));
-benCoin.addTransaction(new Transaction('address2', 'address1', 50));
+// benCoin.addTransaction(new Transaction('address1', 'address2', 150));
+// benCoin.addTransaction(new Transaction('address2', 'address1', 50));
 
 console.log('\nStarting the miner.....');
-benCoin.minePendingTransactions('sweet-address');
+benCoin.minePendingTransactions(myWalletAddress);
 
-console.log('\nBalance of Sweet is ', benCoin.getBalanceOfAddress('sweet-address'));
+console.log('\nBalance of Sweet is ', benCoin.getBalanceOfAddress(myWalletAddress));
 
-console.log('\nStarting the miner again.....');
-benCoin.minePendingTransactions('sweet-address');
+// console.log('\nStarting the miner again.....');
+// benCoin.minePendingTransactions('sweet-address');
 
-console.log('\nBalance of Sweet is ', benCoin.getBalanceOfAddress('sweet-address'));
+// console.log('\nBalance of Sweet is ', benCoin.getBalanceOfAddress('sweet-address'));
