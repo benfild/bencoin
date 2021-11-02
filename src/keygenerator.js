f964f47379762d3e76834d0a24130bec0c6dbbad
benfild <fildben@gmail.com>
2021-11-01 22:00:00 -0400

    modify: public and private key generation in keygenerator.js

--- src/keygenerator.js
+++ src/keygenerator.js
@@ -3,3 +3,8 @@ const ec = new EC('secp256k1');
 
 const key = ec.genKeyPair();
 const publicKey = key.getPublic('hex');
+const privateKey = key.getPrivate('hex');
+
+
+console.log("Private key: ", privateKey);
+console.log("Public key: ", publicKey);
