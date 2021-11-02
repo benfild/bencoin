84834af738dd272aaa64fe10267fdd760b3bea22
benfild <fildben@gmail.com>
2021-11-01 22:24:12 -0400

    modify: update Transaction class with isValid method check null for miningreward return true and throw error when no signature found

--- src/blockchain.js
+++ src/blockchain.js
@@ -16,10 +16,18 @@ class Transaction {
         if(signingKey.getPublic('hex') !== this.fromAddress){
             throw new Error('You cannot sign transactions for other wallets!');
         }
-        
+
         const hashTx = this.calculateHash();
         const sig = signingKey.sign(hashTx, 'base64');
         this.signature = sig.toDER('hex');
+    }
+
+    isValid(){
+        if(this.fromAddress == null) return true;
+
+        if(!this.signature || this.signature.length === 0){
+            throw new Error('No signature in this transaction!');
+        }
     }
 
 
